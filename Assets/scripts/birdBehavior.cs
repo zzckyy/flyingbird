b042ea328dbbc405dfdfdca160044916d3c1be06
zzckyy <ZackyMM2080@gmail.com>
2026-06-16 19:32:46 +0700

    Add bird GameObject and update birdBehavior script for scoring and game over logic

--- Assets/scripts/birdBehavior.cs
+++ Assets/scripts/birdBehavior.cs
@@ -1,24 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class birdBehavior : MonoBehaviour
 {
+    public int score = 0;
+
     public float jumpForce = 100f;
      Rigidbody2D rb;
+
+    bool isHidup;    
+     public Text textSkor;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isHidup = true;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && isHidup)
         {
             rb.AddForce(Vector2.up * jumpForce * 10 * Time.deltaTime, ForceMode2D.Impulse);
+        }
+
+        textSkor.text = score.ToString();
+
+        if(!isHidup){
+            GameOver();
         }
     }
 
@@ -26,8 +39,17 @@ public class birdBehavior : MonoBehaviour
     {
         if (other.gameObject.CompareTag("obstacle"))
         {
-            Debug.Log("Game Over");
-            Time.timeScale = 0f;
+            isHidup = false;
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("pipe")){
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Yoan");
     }
 }
