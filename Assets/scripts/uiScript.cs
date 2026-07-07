9efaaf53ff02359de8f67c7ba371def798af2bf6
zzckyy <ZackyMM2080@gmail.com>
2026-07-07 10:50:00 +0700

    Update quality settings, add main menu scene, and implement UI script
    - Changed the current quality setting to 'WEB' and removed previous quality levels in QualitySettings.asset.
    - Added a new MainMenu.unity scene with basic render and lightmap settings.
    - Created a uiScript.cs to manage game pause, resume, and exit functionalities.
    - Added corresponding meta files for the new scene and script.

---
+++ Assets/scripts/uiScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class uiScript : MonoBehaviour
+{
+    public void PauseGame(){
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame(){
+        Time.timeScale = 1;
+    }
+
+    public void ExitGame(){
+        Application.Quit();
+    }
+
+    
+}
